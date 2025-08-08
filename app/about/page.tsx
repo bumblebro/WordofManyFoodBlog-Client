48c79fbcbc7df3ba7d8872473259e18615b1263d
ShreyasMS <shreyasmadappady20@gmail.com>
2025-08-09 00:39:57 +0530

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -96,18 +96,17 @@ function page() {
                 WordofMany13@gmail.com
               </a>
             </li>
-            /*   <li>
-              📘 <strong>Facebook:</strong>{" "}
-              <a
-                rel="noopener"
-                className="underline"
-                href="https://www.facebook.com/profile.php?id=61574819005948"
-                target="_blank"
-              >
-                facebook.com/profile.php?id=61574819005948
-              </a>
-            </li>
-            */
+              //   <li>
+            //   📘 <strong>Facebook:</strong>{" "}
+            //   <a
+            //     rel="noopener"
+            //     className="underline"
+            //     href="https://www.facebook.com/profile.php?id=61574819005948"
+            //     target="_blank"
+            //   >
+            //     facebook.com/profile.php?id=61574819005948
+            //   </a>
+            // </li>
             <li>
               📌 <strong>Pinterest:</strong>{" "}
               <a
