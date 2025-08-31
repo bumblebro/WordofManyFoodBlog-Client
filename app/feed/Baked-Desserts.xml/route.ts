1af25d77f46071b214431aa69eb3361e0cfece7e
Shreyas M S <shreyasmadappady20@gmail.com>
2025-08-31 10:25:19 +0530

    c

--- app/feed/Baked-Desserts.xml/route.ts
+++ app/feed/Baked-Desserts.xml/route.ts
@@ -1,7 +1,8 @@
 import GETBLOGBYSECTION from "@/app/api/blogbysection/GETBLOGBYSECTION";
 import { generateRSSFeed } from "@/app/utils/generateRSSFeed";
 
-export const revalidate = 0;
+// export const revalidate = 0; // revalidate at most every sec
+export const dynamic = "force-dynamic"; // revalidate at most every sec
 
 export async function GET(request: Request) {
   const recipes = await GETBLOGBYSECTION({ subCategory: "Baked-Desserts" });
